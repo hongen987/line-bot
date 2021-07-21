887c19ac485c6e8c11870dd9dadc7b10845354a6
Allen <0912361632allen@gmail.com>
2021-07-21 21:20:22 +0800

    add stickers

--- app.py
+++ app.py
@@ -37,9 +37,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    rp = event.message.text
+    if '貼圖' in rp:
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=))
 
 
 if __name__ == "__main__":
